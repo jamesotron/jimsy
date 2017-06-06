ca20ad31e2d73c234f7ec6375b7d99abe1319f22
James Harton <james@rabidtech.co.nz>
2017-06-07 09:59:12 +1200

    Make `git clone` work with ssh non-urls.

--- lib/jimsy/git/clone_service.rb
+++ lib/jimsy/git/clone_service.rb
@@ -6,7 +6,7 @@ module Jimsy
       SAFE_HOSTS = %w[github.com bitbucket.org].freeze
 
       def initialize(uri, code_dir)
-        @uri      = URI(uri)
+        @raw_uri  = uri
         @code_dir = File.expand_path(code_dir)
       end
 
@@ -28,7 +28,21 @@ module Jimsy
 
       private
 
-      attr_reader :uri, :code_dir
+      attr_reader :raw_uri, :code_dir
+
+      def uri
+        return @parsed_uri if @parsed_uri
+        return @parsed_uri = parse_git_uri if git_uri?
+        @parsed_uri = URI(raw_uri)
+      end
+
+      def parse_git_uri
+        URI("ssh://#{raw_uri.sub(':', '/')}")
+      end
+
+      def git_uri?
+        raw_uri =~ /^git@/
+      end
 
       def uri_dir
         File.dirname(uri.path)
