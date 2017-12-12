e51c1f88b31dc180c9e5921fbd2643d3d04818d4
James Harton <james@rabidtech.co.nz>
2017-12-13 10:14:40 +1300

    Handle arbitrarily nested project dirs.

--- lib/jimsy/git/clone_service.rb
+++ lib/jimsy/git/clone_service.rb
@@ -3,7 +3,7 @@ require "uri"
 module Jimsy
   class Git
     class CloneService
-      SAFE_HOSTS = %w[github.com bitbucket.org].freeze
+      SAFE_HOSTS = %w[github.com bitbucket.org gitlab.com].freeze
 
       def initialize(uri, code_dir)
         @raw_uri  = uri
@@ -23,12 +23,16 @@ module Jimsy
       end
 
       def target_path
-        @target_path ||= File.join(code_dir, uri.host, uri_dir, uri_repo)
+        @target_path ||= File.join(code_dir, host_dir, project_dir)
       end
 
       private
 
       attr_reader :raw_uri, :code_dir
+
+      def host_dir
+        uri.host.to_s.downcase
+      end
 
       def uri
         return @parsed_uri if @parsed_uri
@@ -44,12 +48,12 @@ module Jimsy
         raw_uri =~ /^git@/
       end
 
-      def uri_dir
-        File.dirname(uri.path)
-      end
-
-      def uri_repo
-        File.basename(uri.path, ".git")
+      def project_dir
+        File.join(Pathname.new(uri.path).each_filename.map do |component|
+          component = component.downcase
+          component = component[0..-5] if component.end_with?(".git")
+          component
+        end)
       end
     end
   end
